d9004fad7ed090a8c7cbf6189ac37471efb39943
DABS\alexg <asan435@stu.columbiacentral.edu>
2018-12-01 00:23:03 -0400

    Changed ViewData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace EducationGotHacked.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page :D";
 
             return View();
         }
